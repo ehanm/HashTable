7c4e7d54160e4c4bd75cc3301655c89f7866551c
ehanm <452735@bsd48.org>
2022-03-12 05:56:29 +0000

    putting stuff in hashtable sort of done

--- studentlist/main.cpp
+++ studentlist/main.cpp
@@ -1,17 +1,54 @@
+
 #include <iostream>
 #include <cstring>
 #include <iomanip>
+#include <fstream>
+#include "node.h"
 
 using namespace std;
 
+void addingtohash(int add, Node* (&student)[100], char* (&first)[1000], char* (&last)[1000], int &idnum);
+
 int main(){
 
+  Node* hashtable[100];
+  
   char input[100];
 
   bool stilladding = true;
   
   cout << "Welcome to Hash Table! This program is the same thing as Studentlist, but uses a Hash method instead!" << endl;
 
+  // set up names
+
+  char* firstname[1000];
+  char* lastname[1000];
+
+  // learned to do from https://www.tutorialspoint.com/how-to-read-a-text-file-with-cplusplus
+  
+  fstream newfile;
+
+  newfile.open("firstname.txt");
+  for (int i = 0; i < 20; i++){
+
+    firstname[i] = new char[80];
+    newfile >> firstname[i];
+
+  }
+
+  newfile.close();
+
+  newfile.open("lastname.txt");
+  for (int i = 0; i < 20; i++){
+
+    lastname[i] = new char[80];
+    newfile >> lastname[i];
+
+  }
+
+  newfile.close();
+
+  int idnum = 0;
 
   while (stilladding == true){
   
@@ -21,8 +58,14 @@ int main(){
 
     if (strcmp(input, "ADD") == 0){
 
-      // add function
+      int adding;
+      
+      cout << "How many students are you adding?" << endl;
 
+      cin >> adding;
+
+      addingtohash(adding, hashtable, firstname, lastname, idnum);
+      
     }
 
     if (strcmp(input, "DELETE") == 0){
@@ -44,4 +87,31 @@ int main(){
     }
   }
 
+}
+
+
+void addingtohash(int add, Node* (&student)[100], char* (&first)[1000], char* (&last)[1000], int &idnum){
+  
+  for (int i = 0; i < add; i++){
+
+    int x = rand() % 20;
+
+    if (student == NULL){
+
+      float grade = rand() % 4; // haha
+
+      idnum++;
+
+      Node* student = new Node();
+      
+      strcpy(student->firstname, first[x]);
+      strcpy(student->lastname, last[x]);
+      
+      student->gpa = grade;
+      
+      student->id = idnum;
+    }
+    
+
+  }
 }
---
+++ studentlist/node.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct Node {
+  
+  char firstname[1000];
+  char lastname[1000];
+
+  int id;
+  float gpa;
+  
+
+};
