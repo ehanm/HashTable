870990e3db1a1b7bf1c097d353a1e4d45a8c2634
ehanm <452735@bsd48.org>
2022-03-17 05:02:18 +0100

    hardest stuff out of the way

--- studentlist/main.cpp
+++ studentlist/main.cpp
@@ -4,17 +4,24 @@
 #include <fstream>
 #include <stdlib.h>
 #include "node.h"
+#include <cstdlib>
 
 using namespace std;
 
-void addingtohash(int &add, Node* (&student)[100], Node* &next, char* (&first)[1000], char* (&last)[1000], int &idnum);
+void addingtohash(int &add, Node* (&student)[100], Node* next, char* (&first)[1000], char* (&last)[1000], int &idnum, int &x, int &y);
 int hashfunction(int idnum);
+void printfunction(Node* hashtable[], int i);
+int randomizer(int x);
 
 int main(){
 
+  Node* head = NULL;
+  
   Node* hashtable[100];
 
   int tablesize = 100;
+
+  srand(time(NULL));
   
   for (int i = 0; i < tablesize; i++){
 
@@ -55,28 +62,38 @@ int main(){
     newlast >> lastnames;
     lastname[i] = lastnames;
   }
-  
-  
-  int idnum = 0;
 
-  while (stilladding == true){
+  int idnum;
+  int adding;
+  Node* next = NULL;
+  Node* current = NULL;
+
+  int x;
+  int y;
+
   
+  while (stilladding == true){
+    
     cout << "What would you like to do? (ADD, DELETE, PRINT, QUIT)" << endl;
 
     cin >> input;
 
     if (strcmp(input, "ADD") == 0){
 
-      int adding;
+      idnum = 0;
       
       cout << "How many students are you adding?" << endl;
 
       cin >> adding;
 
-      Node* next = NULL;
+      while (adding != 0){
 
-      addingtohash(adding, hashtable, next, firstname, lastname, idnum);
-      
+        x = randomizer(x);
+	y = randomizer(y);
+	
+	addingtohash(adding, hashtable, next, firstname, lastname, idnum, x, y);
+	
+      }
     }
 
     if (strcmp(input, "DELETE") == 0){
@@ -86,73 +103,72 @@ int main(){
     }
 
     if (strcmp(input, "PRINT") == 0) {
-
+      
       for (int i = 0; i < tablesize; i++){
 
-	if (hashtable[i] != NULL){
-
-	  Node* temp = hashtable[i];
-
-	  cout << "Name: ";
-	  for (int i = 0; i < 100; i++){
-
-	    cout << temp->firstname[i];
-
-	  }
-
-	  cout << " ";
-	  for (int i = 0; i < 100; i++){
-
-	    cout << temp->lastname[i];
-
-	  }
-
-	  cout << " GPA: ";
-	  
-	  cout << temp->gpa;
-
-	  cout << " ";
-	  
-	  cout << "ID: ";
-	  
-	  cout << temp->id;
-	  
-
-	}
+        printfunction(hashtable, i);
 
       }
-      
-    }
 
+    }
+    
     if (strcmp(input, "QUIT") == 0) {
-
+      
       stilladding = false;
-
-    }
+	  
+    }	
+      
   }
+}
+int randomizer(int x){
+
+  x = (rand() % 20);
+
+  return x;
 
 }
 
+void addmanually(Node* (*student)[100], Node* next){
 
-void addingtohash(int &add, Node* (&student)[100], Node* &next, char* (&first)[1000], char* (&last)[1000], int &idnum){
-
-  int position = hashfunction(idnum);
+  Node* temp = new Node();
   
-  if (add != 0) {
-    for (int i = 0; i < add; i++){
+  cout << "Enter the first name: ";
 
-      srand(time(NULL));
-      
-      int x = (rand() % 20);
+  cin >> temp->firstname;
 
-      srand(time(NULL));
+  cout << "Enter the last name: ";
 
-      int y = (rand() % 20);
-            
+  cin >> temp->lastname;
+
+  cout << "Enter the gpa: ";
+
+  cin >> temp->gpa;
+
+  cout << "Enter the id: ";
+
+  cin >> temp->id;
+
+  int position = hashfunction(temp->id);
+  
+  
+
+
+}
+
+void addingtohash(int &add, Node* (&student)[100], Node* next, char* (&first)[1000], char* (&last)[1000], int &idnum, int &x, int &y){
+  
+  int position = hashfunction(idnum);
+
+  x = randomizer(x);
+
+  cout << x;
+    
+  y = randomizer(y);
+  
+  while (add != 0) {
+                
       if (student[position] == NULL) {
-
-	srand(time(NULL));
-
+	
 	float maxgrade = 4.0;
 
 	float grade = (float)rand()/RAND_MAX * maxgrade;
@@ -160,61 +176,209 @@ void addingtohash(int &add, Node* (&student)[100], Node* &next, char* (&first)[1
 	while (grade > 4){
 
 	  grade--;
-
+	  
 	}
-	
-	cout << grade;
-	
+		
 	idnum++;
-
+	
         Node* newstudent = new Node();
 
 	strcpy(newstudent->firstname, first[x]);
 	strcpy(newstudent->lastname, last[y]);
 
-	for (int i = 0; i < 100; i++){
-	  
-	  cout << newstudent->firstname[i];
-
-	}
-
-	for (int i = 0; i < 100; i++) {
-
-	  cout << newstudent->lastname[i];
-	  
-	}
-
 	newstudent->gpa = grade;
 	
 	newstudent->id = idnum;
-
-	student[position] = newstudent;
-
+	
+	next = newstudent;
+        
+	student[position] = next;
+	
 	add--;
+
+	position = hashfunction(idnum);
+
+	break;
+	
+      }      
+      
+      else if (student[position]->next == NULL) {
+
+        float maxgrade = 4.0;
+
+        float grade = (float)rand()/RAND_MAX * maxgrade;
+
+        while (grade > 4){
+
+          grade--;
+
+        }
+
+        idnum++;
+
+        Node* newstudent = new Node();
+
+        strcpy(newstudent->firstname, first[x]);
+        strcpy(newstudent->lastname, last[y]);
+
+        newstudent->gpa = grade;
+
+        newstudent->id = idnum;
+
+        student[position]->next = newstudent;
+	
+        add--;
+
+	position = hashfunction(idnum);
+
+	break;
       }
 
-      else if (student[position] != NULL) {
+      else if (student[position]->next->next == NULL) {
 
-	addingtohash(add, student, next, first, last, idnum);
-	cout << "does this";
+        float maxgrade = 4.0;
+
+        float grade = (float)rand()/RAND_MAX * maxgrade;
+
+        while (grade > 4){
+
+          grade--;
+
+        }
+
+        idnum++;
+
+        Node* newstudent = new Node();
+
+        strcpy(newstudent->firstname, first[x]);
+        strcpy(newstudent->lastname, last[y]);
+
+        newstudent->gpa = grade;
+
+        newstudent->id = idnum;
+
+        student[position]->next->next = newstudent;
+        add--;
+
+	position = hashfunction(idnum);
+
+	break;
+	
       }
-    }
-    
-    
+
+      
   }
 
+  
+  
 }
 
 int hashfunction(int idnum){
 
-  int hash = (idnum + 26 * 7) % 100;
-
+  int hash = idnum  % 100;
+  
   return hash;
   
 
 }
+ 
+void printfunction(Node* hashtable[], int i){
 
-void print(){
+  if (hashtable[i] != NULL){
+    
+    Node* temp = hashtable[i];
+    
+    cout << "Name: ";
+    for (int i = 0; i < 100; i++){
+      
+      cout << temp->firstname[i];
+	
+    }
+    
+    cout << " ";
+    for (int i = 0; i < 100; i++){
+      
+      cout << temp->lastname[i];
+      
+    }
+    
+    cout << " GPA: ";
+    
+    cout << temp->gpa;
+    
+    cout << " ";
+    
+    cout << "ID: ";
+    
+    cout << temp->id;
+
+    cout << endl;
+    
+    
+    if (hashtable[i]->next != NULL){
+      
+      temp = hashtable[i]->next;
+      
+      cout << "Name: ";
+      for (int i = 0; i < 100; i++){
+	
+	cout << temp->firstname[i];
+	
+      }
+
+      cout << " ";
+      for (int i = 0; i < 100; i++){
+	
+	cout << temp->lastname[i];
+	
+      }
+      
+      cout << " GPA: ";
+      
+      cout << temp->gpa;
+      
+      cout << " ";
+    
+      cout << "ID: ";
+      
+      cout << temp->id;
+
+      cout << endl;
+      
+      if (hashtable[i]->next->next != NULL){
+
+	temp = hashtable[i]->next->next;
+	
+	cout << "Name: ";
+	for (int i = 0; i < 100; i++){
+	  
+	  cout << temp->firstname[i];
+	  
+	}
+	
+	cout << " ";
+	for (int i = 0; i < 100; i++){
+	  
+	  cout << temp->lastname[i];
+	  
+	}
+	
+	cout << " GPA: ";
+    
+	cout << temp->gpa;
+
+	cout << " ";
+	
+	cout << "ID: ";
+	
+	cout << temp->id;
+
+	cout << endl;
 
 
+      }
+      
+    }
+    
+  }
 }
